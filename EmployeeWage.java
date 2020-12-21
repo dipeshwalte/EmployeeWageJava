170152121cea49f6655223a6b0952cbb035f277d
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-21 15:30:44 +0530

    Attendance logic added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,5 +2,12 @@ public class EmployeeWage{
 	public static void main(String[] args)
 	{
 	System.out.println("Welcome to employee wage computation!");
+	int Attendance = (int)Math.floor((Math.random()*10)%2);
+	if(Attendance==1){
+		System.out.println("Employee Present");
+		}
+	else{
+		System.out.println("Employee Absent");
+		}
 	}
 }
