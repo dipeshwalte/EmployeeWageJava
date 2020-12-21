9631249ccd8d21d7e500605cdec1b5f8c4c94321
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-21 15:57:15 +0530

    part time logic added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,13 +5,23 @@ public class EmployeeWage{
 	int attendance = (int)Math.floor((Math.random()*10)%2);
 	int wagePerHour = 20;
 	int fullDayHour = 8;
+	int partTimeHour = 8;
+	int dailyWage = 0;
 	if(attendance==1){
 		System.out.println("Employee Present");
-		System.out.println("Daily Wage ="+(wagePerHour*fullDayHour));
+		dailyWage=(wagePerHour*fullDayHour);
+		System.out.println("Daily Wage: "+dailyWage);
+		int partTime = (int)Math.floor((Math.random()*10)%2);
+		if(partTime==1)
+			{
+			System.out.println("Employee Performed Parttime");
+			dailyWage=dailyWage+(partTimeHour*wagePerHour);
+			System.out.println("New Wage: "+dailyWage);
+			}
 		}
 	else{
 		System.out.println("Employee Absent");
-		System.out.println("Daily Wage = 0");
+		System.out.println("Daily Wage: "+dailyWage);
 		}
 	}
 }
