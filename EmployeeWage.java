b9e233de8056ebd4c5cb1fbbdc91994058faf186
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-21 15:39:22 +0530

    Daily wage logic added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,12 +2,16 @@ public class EmployeeWage{
 	public static void main(String[] args)
 	{
 	System.out.println("Welcome to employee wage computation!");
-	int Attendance = (int)Math.floor((Math.random()*10)%2);
-	if(Attendance==1){
+	int attendance = (int)Math.floor((Math.random()*10)%2);
+	int wagePerHour = 20;
+	int fullDayHour = 8;
+	if(attendance==1){
 		System.out.println("Employee Present");
+		System.out.println("Daily Wage ="+(wagePerHour*fullDayHour));
 		}
 	else{
 		System.out.println("Employee Absent");
+		System.out.println("Daily Wage = 0");
 		}
 	}
 }
