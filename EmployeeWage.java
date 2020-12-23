5d5ce0bdcceebf67ccb01298af16498e7c61283b
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-23 12:43:48 +0530

    While condition added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,30 +2,39 @@ public class EmployeeWage{
 	public static void main(String[] args)
 	{
 	System.out.println("Welcome to employee wage computation!");
-	int attendance = (int)Math.floor((Math.random()*10)%2);
 	int wagePerHour = 20;
 	int fullDayHour = 8;
 	int partTimeHour = 8;
 	int wagesForMonth = 0;
 	int workingDaysPerMonth = 20;
-	for(int i=0;i<20;i++)
+	int workingHours = 0;
+	int workingDays = 0;
+	while(workingDays<20 && workingHours<100)
 		{
+		int attendance = (int)Math.floor((Math.random()*10)%2);
 		int dailyWage = 0;
 		switch(attendance){
 			case 1:
 					System.out.println("Employee Present");
 					dailyWage=(wagePerHour*fullDayHour);
 					wagesForMonth=wagesForMonth + dailyWage;
+					workingHours+=fullDayHour;
+					if(workingHours>100)
+						{
+						continue;
+						}
 					System.out.println("Daily Wage: "+dailyWage);
 					int partTime = (int)Math.floor((Math.random()*10)%2);
 					if(partTime==1)
 						{
 						System.out.println("Employee Performed Parttime");
 						int partTimeWage = (partTimeHour*wagePerHour);
+						workingHours+=fullDayHour;
 						dailyWage=dailyWage+partTimeWage;
 						wagesForMonth= wagesForMonth + partTimeWage;
 						System.out.println("New Daily Wage: "+dailyWage);
 						}
+					workingDays+=1;
 			break;
 			case 0:
 					System.out.println("Employee Absent");
@@ -36,6 +45,8 @@ public class EmployeeWage{
 			break;
 			}//end Switch
 		}//end For
+	System.out.println("Working Hours: "+workingHours);
+	System.out.println("Wordingdays: "+workingDays);
 	System.out.println("Wages for the Month: "+wagesForMonth);
 	}
 }
