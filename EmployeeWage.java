06a688f32be40e3da064402cc0368a715cb3b6df
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-23 12:00:17 +0530

    Switch Case Logic Added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,21 +7,26 @@ public class EmployeeWage{
 	int fullDayHour = 8;
 	int partTimeHour = 8;
 	int dailyWage = 0;
-	if(attendance==1){
-		System.out.println("Employee Present");
-		dailyWage=(wagePerHour*fullDayHour);
-		System.out.println("Daily Wage: "+dailyWage);
-		int partTime = (int)Math.floor((Math.random()*10)%2);
-		if(partTime==1)
-			{
-			System.out.println("Employee Performed Parttime");
-			dailyWage=dailyWage+(partTimeHour*wagePerHour);
-			System.out.println("New Wage: "+dailyWage);
-			}
-		}
-	else{
-		System.out.println("Employee Absent");
-		System.out.println("Daily Wage: "+dailyWage);
+	switch(attendance){
+		case 1:
+				System.out.println("Employee Present");
+				dailyWage=(wagePerHour*fullDayHour);
+				System.out.println("Daily Wage: "+dailyWage);
+				int partTime = (int)Math.floor((Math.random()*10)%2);
+				if(partTime==1)
+					{
+					System.out.println("Employee Performed Parttime");
+					dailyWage=dailyWage+(partTimeHour*wagePerHour);
+					System.out.println("New Wage: "+dailyWage);
+					}
+		break;
+		case 0:
+				System.out.println("Employee Absent");
+				System.out.println("Daily Wage: "+dailyWage);
+		break;
+		default:
+				System.out.println("Something Went Wrong");
+		break;
 		}
 	}
 }
