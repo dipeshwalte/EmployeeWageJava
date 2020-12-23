659793164241dcbad4d5e3a2a8b91e204ca0322f
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-23 16:02:01 +0530

    Added function parameters with mu

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,65 +1,60 @@
 public class EmployeeWage{
-	int wagePerHour = 20;
 	int fullDayHour = 8;
 	int partTimeHour = 8;
-private int computeDailyEmployeeWage()
+private int computeDailyEmployeeWage(int wagePerHour)
 	{
 		return wagePerHour*fullDayHour;
 	}
-private int computePartTimeEmployeeWage()
+private int computePartTimeEmployeeWage(int wagePerHour)
 	{
 		return partTimeHour*wagePerHour;
 	}
-public void computeWagesForMonth()
+public void computeWagesForMonth(int wagePerHour,int maxWorkingDays,int maxWorkingHours)
 	{
 	int wagesForMonth = 0;
-	int workingDaysPerMonth = 20;
 	int workingHours = 0;
 	int workingDays = 0;
-	while(workingDays<20 && workingHours<100)
+	while(workingDays<maxWorkingDays && workingHours<maxWorkingHours)
 		{
 		int attendance = (int)Math.floor((Math.random()*10)%2);
 		int dailyWage = 0;
 		switch(attendance){
 			case 1:
-					System.out.println("Employee Present");
-					dailyWage=computeDailyEmployeeWage();
+					dailyWage=computeDailyEmployeeWage(wagePerHour);
 					wagesForMonth=wagesForMonth + dailyWage;
 					workingHours+=fullDayHour;
 					if(workingHours>100)
 						{
 						continue;
 						}
-					System.out.println("Daily Wage: "+dailyWage);
 					int partTime = (int)Math.floor((Math.random()*10)%2);
 					if(partTime==1)
 						{
-						System.out.println("Employee Performed Parttime");
-						int partTimeWage = computePartTimeEmployeeWage();
+						int partTimeWage = computePartTimeEmployeeWage(wagePerHour);
 						workingHours+=fullDayHour;
 						wagesForMonth= wagesForMonth + partTimeWage;
 						dailyWage=partTimeWage+dailyWage;
-						System.out.println("New Daily Wage: "+dailyWage);
 						}
 					workingDays+=1;
 			break;
 			case 0:
-					System.out.println("Employee Absent");
-					System.out.println("Daily Wage: "+dailyWage);
 			break;
 			default:
 					System.out.println("Something Went Wrong");
 			break;
 			}//end Switch
 		}//end For
-	System.out.println("Working Hours: "+workingHours);
-	System.out.println("Wordingdays: "+workingDays);
 	System.out.println("Wages for the Month: "+wagesForMonth);
 	}
 public static void main(String[] args)
 	{
 	System.out.println("Welcome to employee wage computation!");
 	EmployeeWage wageGenerator = new EmployeeWage();
-	wageGenerator.computeWagesForMonth();
+	System.out.println("Computing Wages for BridgeLabz:");
+	wageGenerator.computeWagesForMonth(30,30,100);
+	System.out.println("Computing Wages for TCS:");
+	wageGenerator.computeWagesForMonth(50,20,100);
+	System.out.println("Computing Wages for Microsoft:");
+	wageGenerator.computeWagesForMonth(100,20,100);
 	}
 }
