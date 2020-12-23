6da29414962120a6bee384ddbf27baa5b00e262d
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-23 12:14:06 +0530

    Monthly wages calculated

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,27 +6,36 @@ public class EmployeeWage{
 	int wagePerHour = 20;
 	int fullDayHour = 8;
 	int partTimeHour = 8;
-	int dailyWage = 0;
-	switch(attendance){
-		case 1:
-				System.out.println("Employee Present");
-				dailyWage=(wagePerHour*fullDayHour);
-				System.out.println("Daily Wage: "+dailyWage);
-				int partTime = (int)Math.floor((Math.random()*10)%2);
-				if(partTime==1)
-					{
-					System.out.println("Employee Performed Parttime");
-					dailyWage=dailyWage+(partTimeHour*wagePerHour);
-					System.out.println("New Wage: "+dailyWage);
-					}
-		break;
-		case 0:
-				System.out.println("Employee Absent");
-				System.out.println("Daily Wage: "+dailyWage);
-		break;
-		default:
-				System.out.println("Something Went Wrong");
-		break;
-		}
+	int wagesForMonth = 0;
+	int workingDaysPerMonth = 20;
+	for(int i=0;i<20;i++)
+		{
+		int dailyWage = 0;
+		switch(attendance){
+			case 1:
+					System.out.println("Employee Present");
+					dailyWage=(wagePerHour*fullDayHour);
+					wagesForMonth=wagesForMonth + dailyWage;
+					System.out.println("Daily Wage: "+dailyWage);
+					int partTime = (int)Math.floor((Math.random()*10)%2);
+					if(partTime==1)
+						{
+						System.out.println("Employee Performed Parttime");
+						int partTimeWage = (partTimeHour*wagePerHour);
+						dailyWage=dailyWage+partTimeWage;
+						wagesForMonth= wagesForMonth + partTimeWage;
+						System.out.println("New Daily Wage: "+dailyWage);
+						}
+			break;
+			case 0:
+					System.out.println("Employee Absent");
+					System.out.println("Daily Wage: "+dailyWage);
+			break;
+			default:
+					System.out.println("Something Went Wrong");
+			break;
+			}//end Switch
+		}//end For
+	System.out.println("Wages for the Month: "+wagesForMonth);
 	}
 }
