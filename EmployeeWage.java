73aa061f447f829f223d8be63ef90e7d55c46df3
dipeshwalte <dipeshwalte@gmail.com>
2021-01-25 19:59:45 +0530

    [Dipesh] modify AddedAllCapsVariable

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,10 @@
 public class EmployeeWage{
 	public static void main(String[] args)
 	{
+	int IS_PRESENT = 1;
 	System.out.println("Welcome to employee wage computation!");
 	int Attendance = (int)Math.floor((Math.random()*10)%2);
-	if(Attendance==1){
+	if(Attendance==IS_PRESENT){
 		System.out.println("Employee Present");
 		}
 	else{
