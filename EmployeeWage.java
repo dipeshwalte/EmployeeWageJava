164e8cd3da7d0440b064c0bba019a9be45871c24
dipeshwalte <dipeshwalte@gmail.com>
2021-01-28 11:51:13 +0530

    [Dipesh] modify RefactoredForConstants

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,30 +1,27 @@
 public class EmployeeWage{
+	static final int WAGE_PER_HOUR = 20;
+	static final int FULL_DAY_HOUR = 8;
+	static final int PART_TIME_HOUR = 4;
+	static final int EMP_FULL_TIME = 1;
+	static final int EMP_PART_TIME = 2;
 	public static void main(String[] args)
 	{
 	System.out.println("Welcome to employee wage computation!");
-	int attendance = (int)Math.floor((Math.random()*10)%2);
-	int wagePerHour = 20;
-	int fullDayHour = 8;
-	int partTimeHour = 8;
+	int attendance = (int)Math.floor((Math.random()*10)%3);
 	int dailyWage = 0;
 	switch(attendance){
 		case EMP_FULL_TIME:
 				System.out.println("Employee Present");
 				dailyWage=(WAGE_PER_HOUR*FULL_DAY_HOUR);
-				WAGES_FOR_MONTH=WAGES_FOR_MONTH + dailyWage;
-				System.out.println("Daily Wage: "+dailyWage);
 		        break;
 		case EMP_PART_TIME:
 				System.out.println("Employee Performed Parttime");
-				int partTimeWage = (PART_TIME_HOUR*WAGE_PER_HOUR);
-				dailyWage=dailyWage+partTimeWage;
-				WAGES_FOR_MONTH= WAGES_FOR_MONTH + partTimeWage;
-				System.out.println("New Daily Wage: "+dailyWage);
+				dailyWage = (PART_TIME_HOUR*WAGE_PER_HOUR);
 			break;
 		default:
 				System.out.println("Employee Absent");
-				System.out.println("Daily Wage: "+dailyWage);
 			break;
 		}//end Switch
-	}
-}
+	System.out.println("Daily Wage: "+dailyWage);
+	}//end main
+}//end Class
