88d6da6eabccd56480180b6d2f13cbc5df5b5756
dipeshwalte <dipeshwalte@gmail.com>
2021-01-28 11:18:59 +0530

    [Dipesh] modify RefactoredForConstants

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,11 +3,12 @@ public class EmployeeWage{
 	{
 	System.out.println("Welcome to employee wage computation!");
 	int attendance = (int)Math.floor((Math.random()*10)%2);
-	int wagePerHour = 20;
-	int fullDayHour = 8;
-	if(attendance==1){
+	int WAGE_PER_HOUR = 20;
+	int FULL_DAY_HOUR = 8;
+	int EMP_PRESENT = 1;
+	if(attendance==EMP_PRESENT){
 		System.out.println("Employee Present");
-		System.out.println("Daily Wage ="+(wagePerHour*fullDayHour));
+		System.out.println("Daily Wage ="+(WAGE_PER_HOUR*FULL_DAY_HOUR));
 		}
 	else{
 		System.out.println("Employee Absent");
